7f96f0bb4092c9a23c17220890a248d2a00f9518
babacar <ndiayebaba007@gmail.com>
2024-01-14 21:50:39 +0100

    add method on repo

--- user-service/src/main/java/com/fst/userservice/repository/RestaurantClientRepository.java
+++ user-service/src/main/java/com/fst/userservice/repository/RestaurantClientRepository.java
@@ -5,5 +5,6 @@ import com.fst.userservice.model.RestaurantClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantClientRepository extends JpaRepository<RestaurantClient, Long> {
-
+    RestaurantClient findByEmail(String email); // execute la requete  -> select * from restaurant_client where email = ?
 }
+
